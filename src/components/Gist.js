05f68d82a267bc82fff34f6f8c139e8c26d03c65
Qasim Hafeez <qasimh556@gmail.com>
2021-06-07 01:03:54 +0400

    setting up Gist

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,3 +1,17 @@
-const Gist = ({ gist }) => {};
+import styled from "styled-components";
+
+const Gist = ({ gist }) => {
+  const { owner } = gist;
+  return (
+    <RootGist>
+      <h2>{owner?.login}</h2>
+    </RootGist>
+  );
+};
+const RootGist = styled.section`
+  h2 {
+    color: red;
+  }
+`;
 
 export default Gist;
