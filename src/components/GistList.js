1851a91e4a56b0ea542ca77d5f197a57facdf371
Qasim Hafeez <qasimh556@gmail.com>
2021-06-07 01:30:19 +0400

    GistLists

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,42 @@
-import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import Gists from "./Gist";
 
-export default GistList
+import useAction from "../actions/gistAction";
+
+const GistList = () => {
+  const { fetchAllGists } = useAction();
+
+  // On page load fetch all the public gists
+  useEffect(() => {
+    fetchAllGists();
+  }, []);
+
+  // Redux Store
+  const gistsPublicList = useSelector((state) => state.gists.gistsPublicList);
+  const singleGistByUserName = useSelector(
+    (state) => state.gists.singleGistByUserName
+  );
+
+  const gistsPublicListError = useSelector(
+    (state) => state.gists.gistsPublicListError
+  );
+
+  return (
+    <RootGistsList>
+      <h6 className="error">{gistsPublicListError && gistsPublicListError}</h6>
+      {gistsPublicList?.map((gist, index) => (
+        <Gist key={index} gist={gist} />
+      ))}
+    </RootGistsList>
+  );
+};
+
+const RootGistsList = styled.section`
+  .error {
+    color: "red";
+  }
+`;
+
+export default GistList;
