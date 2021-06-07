ed9460198d3c42ab6e478626ffb3800b547e8a53
Qasim Hafeez <qasimh556@gmail.com>
2021-06-07 13:26:59 +0400

    Added GistList Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
-import styled from 'styled-components'
+import styled from "styled-components";
 import Header from "./components/Header";
 import GlobalStyles from "./GlobalStyle";
+import GistList from "./components/GistList";
 
 const App = () => {
   return (
     <Wrapper className="App" data-testid="app">
       <Header />
       <GlobalStyles />
+      <GistList />
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   font-size: 14px;
