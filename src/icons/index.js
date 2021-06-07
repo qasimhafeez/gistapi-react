625e84e3253475906cd58e5f72c870db97d156b4
Qasim Hafeez <qasimh556@gmail.com>
2021-06-07 13:58:57 +0400

    svg icons

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+import { ReactComponent as FileIcon } from "./file.svg"
+import { ReactComponent as ForkIcon } from "./fork.svg"
+import { ReactComponent as StarIcon } from "./star.svg"
+import { ReactComponent as CommentsIcon } from "./comments.svg"
+
+export { FileIcon, ForkIcon, StarIcon, CommentsIcon }
